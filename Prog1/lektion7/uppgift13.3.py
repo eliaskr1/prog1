c3a37054c3bd7016034e6c9e38547cda6f5f8f97
Elias Kroon <elias.kroon19@gmail.com>
2023-09-18 16:53:07 +0200

    tog bort en överflödig rad

--- Prog1/lektion7/uppgift13.3.py
+++ Prog1/lektion7/uppgift13.3.py
@@ -18,15 +18,12 @@ Select artist:""")
 cmd = input("> ")
 
 # Söker igenom API1 för att hitta ID till angiven artist
-found = False
-
 for artist in resp_dict1['artists']:
     if artist['name'] == cmd:
         api2 = "https://5hyqtreww2.execute-api.eu-north-1.amazonaws.com/artists/"+ artist['id']
         resp2 = requests.get(api2)
         resp_dict2 = json.loads(resp2.text)
-        found = True
-
+        
 
 # Skapar snyggare strängar att skriva ut
 try:
